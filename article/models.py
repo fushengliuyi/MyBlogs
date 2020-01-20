72e509d46b3e1fb7745a5ede460ab6a25e1f8060
Jack.zhang <995244097@qq.com>
2020-01-20 09:16:20 +0800

    基本模板架构

---
+++ article/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+# 文章
+class Article(models.Model):
+    title = models.CharField(max_length=128)  # 标题
+    content = models.TextField()
+
+    class Meta:
+        db_table = "article"
+    
